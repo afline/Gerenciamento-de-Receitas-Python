ec8dcf05628e913096e127b6ec8954d6076798ac
vinicius <167882525+nogsposito@users.noreply.github.com>
2024-05-17 09:28:14 -0300

    Update cod.py
    Oi gente! vê, eu já consegui fazer a função para visualizar. EU me livrei do counter por quê t ava atrapalhando mais que ajudando e no fundo não realmente servia pra nada. Eu percebi que eu também preciso adaptar a parte dos ingredientes no adicionar para poder ter cada elemento da receita separada (principalmente pra gente ter a funcionalidade das alergias) e eu to trabalhando em que o user possa excluir ou modificar a receita de uma forma que o txt é transformado em uma lista com readlines() e a lista é alterada de acordo com o user e então o texto é reescrito inteiramente com as mudancas feitas pelo user, com writelines(). Essa é a ideia e eu ainda to trabalhando pra deixar tudo certinho

--- cod.py
+++ cod.py
@@ -1,58 +1,81 @@
-def add (counter):
+def add ():
     #essa função adiciona receitas ao código.
+    #funcionando!
     file = open("receitas.txt", "a")
     try:
-        nome = str(input())
-        origem = str(input())
-        ingredientes = str(input())
-        modo = str(input())
+        nome = str(input("Digite o nome da receita: "))
+        origem = str(input("Digite o nome do país de origem da receita: "))
+        ingredientes = str(input("Digite os ingredientes: ")) #ainda deve ser concertado, pois está tudo em só uma string.
+        modo = str(input("Digite o modo de preparo: "))
     except:
         print("Digite apenas com caractéres.")
     else:
         if nome != "x" or origem != "x" or ingredientes != "x" or modo != "x":
-            file.write(f"Receita [{counter}]\n")
-            file.write(f"Nome: {nome}\n")
+            file.write(f"{nome}\n")
             file.write(f"País de origem: {origem}\n")
             file.write(f"Ingredientes: {ingredientes}\n")
             file.write(f"Modo: {modo}\n")
             file.write(f"\n")
             file.close()
+            print()
 
 def viz ():
     #essa serve para vizualizar as receitas.
-    x
+    #funcionando!
+    receita = str(input("Qual receita você deseja vizualizar? "))
+    file = open('receitas.txt', 'r')
+    lista = file.readlines()
+    for i in range (len(lista)):
+        if (lista[i]) == (receita + "\n"):
+            print()
+            print(lista[i])
+            print(lista[i + 1])
+            print(lista[i + 2])
+            print(lista[i + 3])
+    file.close()
 
 def exc ():
     #essa serve para excluir receitas
-    x
+    #AINDA NAO TOTALMENTE FUNCIONAL
+    receita = str(input("Qual receita você deseja excluir? "))
+    file = open('receitas.txt', 'r')
+    lista = file.readlines()
+    print(file.read())
+    file.close()
+    print(lista)
+    for i in range (len(lista)):
+        if (lista[i]) == (receita + "\n"):
+            lista[i] = ''
+            lista[i+1] = ''
+            lista[i+2] = ''
+            lista[i+3] = ''
+    print(lista)
+    file2 = open('receitas.txt', 'w')
+    file2.writelines(lista)
+    file2.close()
 
 def mod ():
     #essa serve para modificar
+    #AINDA NAO FUNCIONAL
     print("Digite [N] para modificar o nome, [O] para o país de origem, [I] para ingredientes e [M] para modo.")
     decisao = str(input("Digite aqui: "))
     if decisao == "N":
-        c
+        print()
     elif decisao == "O":
-        c
+        print()
     elif decisao == "I":
-        c
+        print()
     elif decisao == "M":
-        c
-
-file = open("receitas.txt", "w")
-file.write('')
-file.close()
-#COUNTER AINDA DEVE SER CONSERTADO!
-counter = 1
+        print()
 
 while True:
     #serve apenas para servir como uma interface pro user decidir o que deseja fazer no programa.
+    #me livrei do counter xD
     print("Digite [A] para adicionar receita, [V] para vizualizar, [M] para modificar, [E] para excluir e [Q] para quitar.")
     decisao = str(input("Digite aqui: "))
     print()
     if decisao == "A":
-        add(counter)
-        counter+=1
+        add()
     elif decisao == "V":
         viz()
     elif decisao == "M":
