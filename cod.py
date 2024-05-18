32ecf5dbfbaee53fc124b579cf8e1a32cb4d3f92
vinicius <167882525+nogsposito@users.noreply.github.com>
2024-05-18 12:59:31 -0300

    Update cod.py
    Mais um commit! O modificar e o excluir ainda não estão perfeitos mas eu já to trabalhando nisso. Além disso, falta um pouco mais de tratamento de erro e organizar a interface do arquivo para que não fiquem uns \n aleatórios. Mas to quase concluindo , também deixei todas as informaçoes sempre em uppercase para evitar problemas.

--- cod.py
+++ cod.py
@@ -3,27 +3,31 @@ def add ():
     #funcionando!
     while True:
         print("Digite [SAIR] se desejar voltar para a tela inicial e cancelar a ação. Digite qualquer outra coisa para continuar.")
-        x = str(input("Aqui: "))
+        x = input("Digite aqui: ").upper()
         if x == "SAIR":
             break
         print()
         file = open("receitas.txt", "a")
         try:
-            nome = str(input("Digite o nome da receita: "))
-            origem = str(input("Digite o nome do país de origem da receita: "))
-            ingredientes = str(input("Digite os ingredientes: ")) #ainda deve ser concertado, pois está tudo em só uma string.
-            modo = str(input("Digite o modo de preparo: "))
+            nome = str(input("Digite o nome da receita: ")).upper()
+            origem = str(input("Digite o nome do país de origem da receita: ")).upper()
+            ingredientes = str(input("Digite os ingredientes: ")).upper() #ainda deve ser concertado, pois está tudo em só uma string.
+            modo = str(input("Digite o modo de preparo: ")).upper()
         except:
             print("Digite apenas com caractéres.")
         else:
             if nome != "x" or origem != "x" or ingredientes != "x" or modo != "x":
                 file.write(f"Nome: {nome}\n")
                 file.write(f"País de origem: {origem}\n")
-                file.write(f"Ingredientes: {ingredientes}\n")
-                file.write(f"Modo: {modo}\n")
+                file.write(f"Ingredientes (Digite os ingredientes separados em vírgulas e espaço.): {ingredientes}\n")
+                file.write(f"Modo de preparo: {modo}\n")
                 file.write(f"\n")
                 file.close()
                 print()
+                print("Adicionado com sucesso!")
+                print()
+            else:
+                print("Não disponível!")
         break
 
 def viz ():
@@ -31,11 +35,11 @@ def viz ():
     #funcionando!
     while True:
         print("Digite [SAIR] se desejar voltar para a tela inicial e cancelar a ação. Digite qualquer outra coisa para continuar.")
-        x = str(input("Aqui: "))
+        x = input("Digite aqui: ").upper()
         if x == "SAIR":
             break
         print()
-        receita = str(input("Qual receita você deseja vizualizar? "))
+        receita = str(input("Qual receita você deseja vizualizar? ")).upper()
         file = open('receitas.txt', 'r')
         lista = file.readlines()
         for i in range (len(lista)):
@@ -50,23 +54,23 @@ def viz ():
 
 def exc ():
     #essa serve para excluir receitas
-    #funcionando!
+    #o lista.pop de vez em quando da index out of range
     while True:
         print("Digite [SAIR] se desejar voltar para a tela inicial e cancelar a ação. Digite qualquer outra coisa para continuar.")
-        x = str(input("Aqui: "))
+        x = input("Digite aqui: ").upper()
         if x == "SAIR":
             break
         print()
-        receita = str(input("Qual receita você deseja excluir? "))
+        receita = str(input("Qual receita você deseja excluir? ")).upper()
         file = open('receitas.txt', 'r')
         lista = file.readlines()
         file.close()
         for i in range (len(lista)):
             if (lista[i]) == ("Nome: "+ receita + "\n"):
-                lista[i] = ''
-                lista[i+1] = ''
-                lista[i+2] = ''
-                lista[i+3] = ''
+                lista.pop(i)
+                lista.pop(i+1)
+                lista.pop(i+2)
+                lista.pop(i+3)
                 file2 = open('receitas.txt', 'w')
                 file2.write('')
                 file2.writelines(lista)
@@ -77,34 +81,34 @@ def exc ():
 
 def mod ():
     #essa serve para modificar
-    #AINDA NAO FUNCIONAL
+    #funcionando na maior parte, mas fica repetindo no loop por algum motivo
     while True:
         print("Digite [SAIR] se desejar voltar para a tela inicial e cancelar a ação. Digite qualquer outra coisa para continuar.")
-        x = str(input("Aqui: "))
+        x = input("Digite aqui: ").upper()
         print()
         if x == "SAIR":
             break
-        receitaMod = str(input("Qual receita você deseja modificar? "))
+        receitaMod = str(input("Qual receita você deseja modificar? ")).upper()
         file = open('receitas.txt', 'r')
         lista = file.readlines()
         file.close()
         for i in range (len(lista)):
-            if (lista[i]) == (receitaMod + "\n"):
+            if (lista[i]) == ("Nome: " + receitaMod + "\n"):
                 print(lista)
-                print(receitaMod)
+                print("Nome: " + receitaMod + "\n")
                 print("Digite [N] para modificar o nome, [P] para o país de origem, [I] para ingredientes e [M] para modo.")
-                decisao = str(input("Digite aqui: "))
+                decisao = str(input("Digite aqui: ")).upper()
                 print("Digite a nova informação: ")
-                novaInfo = str(input("Digite aqui: "))
+                novaInfo = str(input("Digite aqui: ")).upper()
 
                 if decisao == "N":
                     lista[i] = (f"{novaInfo}\n")
                 elif decisao == "P":
                     lista[i+1] = (f"País de origem: {novaInfo}\n")
                 elif decisao == "I":
-                    lista[i+2] = (f"Ingredientes: {novaInfo}\n")
+                    lista[i+2] = (f"Ingredientes (Digite os ingredientes separados em vírgulas e espaço.): {novaInfo}\n")
                 elif decisao == "M":
-                    lista[i+3] = (f"Modo: {novaInfo}\n")
+                    lista[i+3] = (f"Modo de preparo: {novaInfo}\n")
 
                 print(lista)
                 file2 = open('receitas.txt', 'w')
@@ -118,7 +122,7 @@ while True:
     #serve apenas para servir como uma interface pro user decidir o que deseja fazer no programa.
     print()
     print("Digite [A] para adicionar receita, [V] para vizualizar, [M] para modificar, [E] para excluir e [Q] para quitar.")
-    decisao = input("Digite aqui: ").upper() #independente de digitar a ou A, o programa roda. também planejo fazer isso com as receitas para evitar problemas.
+    decisao = input("Digite aqui: ").upper()
     print()
     if decisao == "A":
         add()
